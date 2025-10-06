9f867ddc467e02facd0214821f1f5f6b36d79b5f
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:26:19 +0100

    Update NJobMain.java : bump nuts 0.8.7

--- njob/src/main/java/net/thevpc/nuts/toolbox/njob/NJobMain.java
+++ njob/src/main/java/net/thevpc/nuts/toolbox/njob/NJobMain.java
@@ -1,8 +1,11 @@
 package net.thevpc.nuts.toolbox.njob;
 
-import net.thevpc.nuts.*;
+import net.thevpc.nuts.app.NApp;
+import net.thevpc.nuts.app.NAppDefinition;
+import net.thevpc.nuts.app.NAppRunner;
 import net.thevpc.nuts.cmdline.NArg;
 import net.thevpc.nuts.cmdline.NCmdLine;
+import net.thevpc.nuts.core.NSession;
 
 @NAppDefinition
 public class NJobMain {
