f0d1484c7e8a4a545fe4fcf9a16e5e4d806ed0ce
thevpc <taha.bensalah@gmail.com>
2025-10-06 04:26:19 +0100

    Update NCompaniesSubService.java : bump nuts 0.8.7

--- njob/src/main/java/net/thevpc/nuts/toolbox/njob/NCompaniesSubService.java
+++ njob/src/main/java/net/thevpc/nuts/toolbox/njob/NCompaniesSubService.java
@@ -1,6 +1,6 @@
 package net.thevpc.nuts.toolbox.njob;
 
-import net.thevpc.nuts.NSession;
+import net.thevpc.nuts.core.NSession;
 
 public class NCompaniesSubService {
     private NSession session;
